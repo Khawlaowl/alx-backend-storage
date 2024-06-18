bff714c0f3a91eda655956757e9a3c8ac938ca87
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2024-06-19 00:24:41 +0100

    f

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -24,14 +24,3 @@ def top_students(mongo_collection):
         ]
     )
     return students
-
-def main():
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.logs
-    collection = db.students
-
-    for student in top_students(collection):
-        print(f"Name: {student['name']}, Average Score: {student['averageScore']}")
-
-if __name__ == '__main__':
-    main()
